9af5f22ab1052be284f197206bb14a816589746f
Felipe Erik <felipeerik1524@gmail.com>
2026-07-02 23:14:18 -0300

    chore: adicionando Django REST Framework e configurações básicas

--- config/settings.py
+++ config/settings.py
@@ -43,8 +43,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'rest_framework',
     'finance_cookie',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
