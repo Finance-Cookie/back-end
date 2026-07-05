f63cfb448ea7aca055b58d396563d6de890bac3d
Felipe Erik <felipeerik1524@gmail.com>
2026-07-04 21:43:24 -0300

    feat: adicionando filtros de busca e ordenação na API

--- finance_cookie/urls.py
+++ finance_cookie/urls.py
@@ -1,19 +1,19 @@
 from django.urls import path, include
 from rest_framework import routers
-from . import api_views
+from . import views
 
 router = routers.DefaultRouter()
-router.register(r'clientes', api_views.ClienteViewSet, basename='clientes')
-router.register(r'produtos', api_views.ProdutoViewSet, basename='produtos')
-router.register(r'items', api_views.ItemViewSet, basename='items')
-router.register(r'tipos', api_views.TipoPagamentoViewSet, basename='tipospagamento')
-router.register(r'formas', api_views.FormaPagamentoViewSet, basename='formaspagamento')
-router.register(r'entradas', api_views.EntradaViewSet, basename='entradas')
-router.register(r'saidas', api_views.SaidaViewSet, basename='saidas')
-router.register(r'compras', api_views.CompraViewSet, basename='compras')
-router.register(r'itemcompras', api_views.ItemCompraViewSet, basename='itemcompras')
-router.register(r'vendas', api_views.VendaViewSet, basename='vendas')
-router.register(r'produtovenda', api_views.ProdutoVendaViewSet, basename='produtovenda')
+router.register(r'clientes', views.ClienteViewSet, basename='clientes')
+router.register(r'produtos', views.ProdutoViewSet, basename='produtos')
+router.register(r'items', views.ItemViewSet, basename='items')
+router.register(r'tipos', views.TipoPagamentoViewSet, basename='tipospagamento')
+router.register(r'formas', views.FormaPagamentoViewSet, basename='formaspagamento')
+router.register(r'entradas', views.EntradaViewSet, basename='entradas')
+router.register(r'saidas', views.SaidaViewSet, basename='saidas')
+router.register(r'compras', views.CompraViewSet, basename='compras')
+router.register(r'itemcompras', views.ItemCompraViewSet, basename='itemcompras')
+router.register(r'vendas', views.VendaViewSet, basename='vendas')
+router.register(r'produtovenda', views.ProdutoVendaViewSet, basename='produtovenda')
 
 urlpatterns = [
 	path('', include(router.urls)),
